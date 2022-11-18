f633153cd48209dbfa5bd5f48b799fd42f984ef7
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-18 20:49:12 +0600

    started working on express cafe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-4xl text-center">Lets build Express Cafe</h1>
+      <button className="btn btn-primary">Do it</button>
     </div>
   );
 }
